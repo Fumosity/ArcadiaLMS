e6a3d15c6138b367f5d9778cf228f2dcc8f9e049
222tilnini <tdgag8@gmail.com>
2024-10-10 14:36:33 +0800

    Hays Linus

--- arcadia-project/src/components/admin-book-inventory-comp/InventoryTitle.jsx
+++ arcadia-project/src/components/admin-book-inventory-comp/InventoryTitle.jsx
@@ -4,7 +4,7 @@ const InventoryTitle = () => (
     <div>
       <div className="flex items-center mb-6">
         <img src="image/arcadia.png" alt="Book icon" className="h-6 w-6 mr-2" />
-        <h2 className="text-2xl font-semibold">Book Inventory for Admin</h2>
+        <h2 className="text-2xl font-semibold">Book Inventory</h2>
       </div>
     </div>
    
