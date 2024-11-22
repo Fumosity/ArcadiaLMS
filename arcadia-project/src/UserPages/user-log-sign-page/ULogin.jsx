83b479188c873e683a317e2102941f933d860ba0
222tilnini <tdgag8@gmail.com>
2024-11-22 10:02:44 +0800

    Update ULogin.jsx

--- arcadia-project/src/UserPages/user-log-sign-page/ULogin.jsx
+++ arcadia-project/src/UserPages/user-log-sign-page/ULogin.jsx
@@ -1,19 +1,6 @@
 import React from "react";
 import UNavbar from "../../components/UserComponents/user-main-comp/UNavbar";
-import UsearchBar from "../../components/UserComponents/user-main-comp/USerachBar";
 import Title from "../../components/main-comp/Title";
-
-import SimBooks from "../../components/UserComponents/user-book-catalog-comp/SimBooks";
-
-import ArcOpHr from "../../components/UserComponents/user-home-comp/ArcOpHr";
-import UpEvents from "../../components/UserComponents/user-home-comp/UpEvents";
-import Services from "../../components/UserComponents/user-main-comp/Services";
-import UHero from "../../components/UserComponents/user-home-comp/UHero";
-import LibServ from "../../components/UserComponents/user-serv-comp/LibServ";
-import SupportCont from "../../components/UserComponents/user-support-comp/SupportCont";
-import ContactUs from "../../components/UserComponents/user-support-comp/ContactUs";
-import FAQSection from "../../components/UserComponents/user-faq-comp/FAQSection";
-import LoginCont from "../../components/UserComponents/user-login-sign-up-comp/LoginForm";
 import LoginForm from "../../components/UserComponents/user-login-sign-up-comp/LoginForm";
 
 const ULogin = () => {
