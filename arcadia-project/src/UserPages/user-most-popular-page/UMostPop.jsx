1b85314989261015c1bbc2c01b7d30bf450014aa
ariexxvi <119130073+ariexxvi@users.noreply.github.com>
2025-04-03 01:56:32 +0800

    Update UMostPop.jsx

--- arcadia-project/src/UserPages/user-most-popular-page/UMostPop.jsx
+++ arcadia-project/src/UserPages/user-most-popular-page/UMostPop.jsx
@@ -6,7 +6,7 @@ import ArcOpHr from "../../components/UserComponents/user-home-comp/ArcOpHr";
 import UpEvents from "../../components/UserComponents/user-home-comp/UpEvents";
 import Services from "../../components/UserComponents/user-main-comp/Services";
 import MostPopBk from "../../components/UserComponents/user-main-comp/MostPopBk";
-import HighestRatedBk from "../../components/UserComponents/user-main-comp/HIghestRatedBk";
+import HighestRatedBk from "../../components/UserComponents/user-main-comp/HighestRatedBk";
 import Title from "../../components/main-comp/Title";
 import ExpandedMostPop from "../../components/UserComponents/user-most-popular-comp/ExpandedMostPop";
 
