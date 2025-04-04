30204bd2340414b5606d515d199b9c4f259cefaf
ariexxvi <119130073+ariexxvi@users.noreply.github.com>
2025-04-04 19:41:55 +0800

    Update vite.config.js

--- arcadia-project/vite.config.js
+++ arcadia-project/vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   build: {
     outDir: "dist",
-    assetsDir: "assets"
+    assetsDir: "assets",
+    cssCodeSplit: true,  
   },
   plugins: [react()],
   optimizeDeps: {
