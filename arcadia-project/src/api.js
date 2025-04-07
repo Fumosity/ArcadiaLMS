d9fdce7ef9d07f6a81b74a3ceeffe8c5d3783352
222tilnini <tdgag8@gmail.com>
2025-04-07 22:28:11 +0800

    Update api.js

--- arcadia-project/src/api.js
+++ arcadia-project/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const BASE_URL = "http://13.212.24.184"
+export const BASE_URL = "http://47.128.154.197"
 export const API_URL = `${BASE_URL}/api`
 
 const api = axios.create({
