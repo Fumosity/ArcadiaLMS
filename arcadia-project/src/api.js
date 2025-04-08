448f3e39e6c509515b625004fd2ceab4bb6dae3a
222tilnini <tdgag8@gmail.com>
2025-04-08 11:16:25 +0800

    test

--- arcadia-project/src/api.js
+++ arcadia-project/src/api.js
@@ -2,11 +2,10 @@ import axios from "axios"
 import config from "./config.json"
 
 export const BASE_URL = config.BASE_URL
-export const API_URL = `${BASE_URL}/api`
+export const API_URL = `${BASE_URL}`
 
 const api = axios.create({
   baseURL: API_URL,
 })
 
 export default api
-
