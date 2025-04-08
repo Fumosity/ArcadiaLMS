7672b1803ef55f5d9a751a6bf8ae59405c71f00e
222tilnini <tdgag8@gmail.com>
2025-04-08 10:25:42 +0800

    Update Onboarding.jsx

--- arcadia-project/src/components/UserComponents/user-login-sign-up-comp/Onboarding.jsx
+++ arcadia-project/src/components/UserComponents/user-login-sign-up-comp/Onboarding.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { supabase } from "../../../supabaseClient.js"
 import { useUser } from "../../../backend/UserContext.jsx"
-import { API_URL} from "../../../api.js"
+import { API_URL } from "../../../api.js"
 import { toast } from "react-toastify"
 
 export default function Onboarding({ userData, selectedGenres }) {
@@ -59,7 +59,6 @@ export default function Onboarding({ userData, selectedGenres }) {
 
             const newUserID = userInsertData[0].userID
             console.log("New userID:", newUserID)
-            console.log("BASE_URL", BASE_URL)
 
             // Send verification email - use API_URL to ensure correct endpoint
             const response = await fetch(`${API_URL}/send-email`, {
