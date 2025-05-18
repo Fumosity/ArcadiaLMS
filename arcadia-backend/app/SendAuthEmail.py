cab86964ec112088a37ca3b3f2aab954b81d484a
222tilnini <tdgag8@gmail.com>
2025-05-18 18:52:17 +0800

    Update SendAuthEmail.py

--- arcadia-backend/app/SendAuthEmail.py
+++ arcadia-backend/app/SendAuthEmail.py
@@ -69,12 +69,12 @@ def send_verification_email(to_email: str, first_name: str, token: str):
     msg["To"] = to_email
 
     # Load and attach the image
-    image_path = Path("arcadia-project\public\image\emailimage.png")
+    image_path = Path("arcadia-project/public/image/emailimage.png")
     with image_path.open("rb") as img:
         msg.get_payload()[1].add_related(
             img.read(),
             maintype='image',
-            subtype='jpeg',
+            subtype='png',
             cid=image_cid
         )
 
